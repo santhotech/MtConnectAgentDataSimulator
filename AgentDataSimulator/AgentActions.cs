3cecf8a2ae79598105a62ea6fbdc9fb5181d365d
Santhosh Kumar <santho89@gmail.com>
2011-08-24 17:13:09 +0530

    added a method agent start to handle the agent start action

--- AgentDataSimulator/AgentActions.cs
+++ AgentDataSimulator/AgentActions.cs
@@ -24,5 +24,24 @@ namespace AgentDataSimulator
                 }
             }
         }
+        public void AgentStart()
+        {
+            ProcessStartInfo pr1 = new ProcessStartInfo();
+            pr1.FileName = Properties.Settings.Default.agtFile;
+            pr1.WindowStyle = ProcessWindowStyle.Hidden;
+            string xmltName = Properties.Settings.Default.xmlFile;
+            string listName = Properties.Settings.Default.lstFile;
+            pr1.Arguments = "-f " + xmltName + " -c " + listName;
+            try
+            {
+                p1 = Process.Start(pr1);
+                this.AgtFlag = true;
+            }
+            catch
+            {
+                this.AgtFlag = false;
+                MessageBox.Show("Cannot start agent", "Error");
+            }
+        }
     }
 }
