45de80cb673c52ebfc70402c43c0db60f3c329a7
Santhosh Kumar <santho89@gmail.com>
2011-08-24 17:35:51 +0530

    added a class methods to wrap all utilitiy methods

---
+++ AgentDataSimulator/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgentDataSimulator
+{
+    class Methods
+    {
+        bool IsAllDigits(string s)
+        {
+            foreach (char c in s)
+            {
+                if (!Char.IsDigit(c))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
