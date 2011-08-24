501809c0fc0417fbfd2f49bf958ea02e69cab640
Santhosh Kumar <santho89@gmail.com>
2011-08-24 17:23:32 +0530

    added methods to access textbox, buttons and listview controls in a thread safe manner

--- AgentDataSimulator/ThreadSafeUIAccessors.cs
+++ AgentDataSimulator/ThreadSafeUIAccessors.cs
@@ -23,5 +23,70 @@ namespace AgentDataSimulator
 {
     class ThreadSafeUIAccessors
     {
+        public void SetText(string text, Control ctrl)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                ctrl.Invoke(d, new object[] { text, ctrl });
+            }
+            else
+            {
+                if (ctrl.GetType() == typeof(Label))
+                {
+                    ctrl.Text = text;
+                }
+                else
+                {
+                    ctrl.Text += Environment.NewLine + text;
+                }
+            }
+        }
+
+        public void SetEnabled(Control ctrl, bool a)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                SetEnabledCallback d = new SetEnabledCallback(SetEnabled);
+                ctrl.Invoke(d, new object[] { ctrl, a });
+            }
+            else
+            {
+                ctrl.Enabled = a;
+            }
+        }
+
+        public void SetCtrlCol(Control ctrl, Color col, int backOrFront)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                SetCtrlColCallback d = new SetCtrlColCallback(SetCtrlCol);
+                ctrl.Invoke(d, new object[] { ctrl, col, backOrFront });
+            }
+            else
+            {
+                if (backOrFront == 0)
+                {
+                    ctrl.BackColor = col;
+                }
+                if (backOrFront == 1)
+                {
+                    ctrl.ForeColor = col;
+                }
+            }
+        }
+
+        public void AddItemToList(ListViewItem itm, ListView lst)
+        {
+            if (lst.InvokeRequired)
+            {
+                AccessControlListCallback d = new AccessControlListCallback(AddItemToList);
+                lst.Invoke(d, new object[] { itm, lst });
+            }
+            else
+            {
+                lst.Items.Add(itm);
+            }
+        }     
     }
 }
